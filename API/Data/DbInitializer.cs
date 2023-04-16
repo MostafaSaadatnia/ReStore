242980abed494f5f55ebad5bd4d0e3e305ea2a26
mostafasaadatnia <mostafasaadatnia@gmail.com>
2023-04-16 03:22:24 -0700

    add data seed

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -1,7 +1,49 @@
+using API.Entities;
+
 namespace API.Data
 {
-    public class DbInitializer
+    public static class DbInitializer
     {
-        
+        public static void Initialize(StoreContext context)
+        {
+            if (context.Products.Any()) return;
+
+            var products = new List<Product>(){
+                new Product {
+                    Name= "Angular Blue Boots",
+                    Description = "very long lorem ipsum about angular description has been created with me!",
+                    Price=10000,
+                    PictureUrl="/images/products/boot-ang.png",
+                    Brand="Angular",
+                    Type="Boots",
+                    QualityInStock=100
+                },
+                                new Product {
+                    Name= "Angular Blue Boots",
+                    Description = "very long lorem ipsum about angular description has been created with me!",
+                    Price=10000,
+                    PictureUrl="/images/products/boot-ang.png",
+                    Brand="Angular",
+                    Type="Boots",
+                    QualityInStock=100
+                },
+                                new Product {
+                    Name= "Angular Blue Boots",
+                    Description = "very long lorem ipsum about angular description has been created with me!",
+                    Price=10000,
+                    PictureUrl="/images/products/boot-ang.png",
+                    Brand="Angular",
+                    Type="Boots",
+                    QualityInStock=100
+                }
+            };
+
+            foreach (var product in products)
+            {
+                context.Products.Add(product);
+            }
+
+            context.SaveChanges();
+        }
     }
 }
